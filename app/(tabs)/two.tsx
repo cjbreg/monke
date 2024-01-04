851891ed78d38541029ecb86eab9ef992655f307
Coen Breg <coenbreg@gmail.com>
2024-01-04 10:28:07 +0100

    chore: test fingerprint package

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,24 +1,59 @@
-import {Box, Divider, Heading} from '@gluestack-ui/themed';
-import Palette from '../../src/components/cheatsheet/Palette';
+import * as LocalAuthentication from 'expo-local-authentication';
+import {Box, Heading} from '@gluestack-ui/themed';
+import {useEffect, useState} from 'react';
+import {TouchableOpacity} from 'react-native';
 
 
 
 export default function TabTwoScreen() {
+    const [isBiometricSupported, setIsBiometricSupported] = useState(false);
+    const [fingerprint, setFingerprint] = useState(false);
+
+    useEffect(() => {
+        (async() => {
+            const compatible = await LocalAuthentication.hasHardwareAsync();
+            setIsBiometricSupported(compatible);
+            const enroll = await LocalAuthentication.isEnrolledAsync();
+            if (enroll) {
+                setFingerprint(true);
+            }
+        })();
+    }, []);
+
+    const handle = async() => {
+        try {
+            const biometricAuth = await LocalAuthentication.authenticateAsync({
+                promptMessage: 'Login with Fingerprint',
+                disableDeviceFallback: true,
+                cancelLabel: 'Cancel',
+            });
+            if (biometricAuth.success) {
+                // router.replace('Home');
+                console.log('success');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
 
         <Box
             flex={1}
             alignItems="center"
             justifyContent="center"
+            bgColor="$latteFoam"
+            $dark-bg="$coffeeQuaternary"
         >
-            <Heading>Color Palette</Heading>
-
-            <Palette />
-
-            <Divider
-                my='$8'
-                w='80%'
-            />
+            {isBiometricSupported && fingerprint ? (
+                <TouchableOpacity onPress={handle}>
+                    <Heading>Start fingerprint test</Heading>
+                </TouchableOpacity>
+            ) : (
+                <Box>
+                    <Heading>fingerprint not supported/ allocated</Heading>
+                </Box>
+            )}
         </Box>
     );
 }
