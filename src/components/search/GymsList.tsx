7b56e2a14ac7e80732ed82855e9fcacb46eee489
Coen Breg <coenbreg@gmail.com>
2024-02-16 16:18:39 +0100

    feat: implemented basic fuzzy search

--- src/components/search/GymsList.tsx
+++ src/components/search/GymsList.tsx
@@ -1,7 +1,8 @@
-import {Box, Divider} from '@gluestack-ui/themed';
+import {Box, Divider, Input, InputField} from '@gluestack-ui/themed';
 import {FlashList, ListRenderItem} from '@shopify/flash-list';
-import React, {useCallback, useMemo} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import ColoredRefreshControl from '../layout/ColoredRefreshControl';
+import FuzzySearch from 'fuzzy-search';
 import GymListItem from './GymListItem';
 import {GymPreview} from '@cjbreg/toplogger-sdk';
 import LoadingView from '../layout/LoadingView';
@@ -15,12 +16,22 @@ interface GymListProps {
 const GymsList = (props: GymListProps) => {
     const {gyms, isLoading, isUpdating, refreshGyms} = useGymsHook();
     const {setGymId, gym} = useGym();
-
+    const [searchQuery, setSearchQuery] = useState<string | null>('' as string);
     const {onDismiss} = props;
-
 
     const activeGyms = useMemo(() => gyms.filter(gym => gym.live === true), [gyms]);
     const sortedGyms = useMemo(() => activeGyms.sort((a, b) => a.name.localeCompare(b.name)), [activeGyms]);
+
+    const searcher = useMemo(() => new FuzzySearch(sortedGyms, ['name', 'city'], {
+        sort: true,
+    }), [sortedGyms]);
+
+    const searchedGyms = useMemo(() => {
+        if (searchQuery) {
+            return searcher.search(searchQuery);
+        }
+        return sortedGyms;
+    }, [searcher, searchQuery, sortedGyms]);
 
     const handleSelectGym = useCallback((gymId: number) => {
         setGymId(gymId);
@@ -55,23 +66,34 @@ const GymsList = (props: GymListProps) => {
         <LoadingView
             isLoading={isLoading}
             px={0}
+            pt={0}
         >
-            <FlashList
-                showsVerticalScrollIndicator={false}
-                data={sortedGyms}
-                renderItem={_renderItems}
-                keyExtractor={(item) => item.id.toString()}
-                ItemSeparatorComponent={_renderSeparator}
-                estimatedItemSize={67}
-                refreshControl={
-                    <ColoredRefreshControl
-                        refreshing={isUpdating}
-                        onRefresh={() => {
-                            refreshGyms();
+            <Box flex={1} gap="$4" >
+                <Input >
+                    <InputField placeholder="Search for a gym"
+                        onChangeText={(text) => {
+                            setSearchQuery(text);
                         }}
                     />
-                }
-            />
+                </Input>
+
+                <FlashList
+                    showsVerticalScrollIndicator={false}
+                    data={searchedGyms}
+                    renderItem={_renderItems}
+                    keyExtractor={(item) => item.id.toString()}
+                    ItemSeparatorComponent={_renderSeparator}
+                    estimatedItemSize={67}
+                    refreshControl={
+                        <ColoredRefreshControl
+                            refreshing={isUpdating}
+                            onRefresh={() => {
+                                refreshGyms();
+                            }}
+                        />
+                    }
+                />
+            </Box>
         </LoadingView>
     );
 };
